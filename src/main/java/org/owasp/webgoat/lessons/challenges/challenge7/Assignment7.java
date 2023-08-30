b92eb8700f2e1511d23d0fdfe4e67b1ee2ad5381
smaguilarcx <75253702+smaguilarcx@users.noreply.github.com>
2023-08-30 11:02:31 +0200

    Update Assignment7.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge7/Assignment7.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge7/Assignment7.java
@@ -31,7 +31,7 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class Assignment7 extends AssignmentEndpoint {
 
-  public static final String ADMIN_PASSWORD_LINK = "375afe1104f4a487a73823c50a9292a2";
+  public static final String ADMIN_PASSWORD_LINK = "";
 
   private static final String TEMPLATE =
       "Hi, you requested a password reset link, please use this <a target='_blank'"
